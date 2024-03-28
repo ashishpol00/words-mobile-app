8a0833c2f1bd8601847e7b28a3ef16ed4b69b1b1
ashishpol00 <ashishrameshpol4@gmail.com>
2024-03-28 16:00:24 +0530

    feat: logic of adding new word and editing existing word and deleting multiple words added

--- src/_stores/_types/words-store-types.ts
+++ src/_stores/_types/words-store-types.ts
@@ -2,4 +2,9 @@ import type { Word } from '../../app/_shared/types/types';
 
 export type WordsStore = {
     words: Word[];
+
+    loadWordsToWordsStoreFromDB: () => void;
+    addWord: (word: Word) => void;
+    editWord: (word: Word) => void;
+    deleteWords: (spellingsOfwordsToDelete: string[]) => void;
 };
--- src/_stores/words-store/main.ts
+++ src/_stores/words-store/main.ts
@@ -1,10 +1,44 @@
 import type { StateCreator } from 'zustand';
 import type { WordsStore } from '../_types/words-store-types';
+import { showToastMessage } from '../../app/_shared/utils/toaster';
 
 export const __wordsStoreStateCreator: StateCreator<WordsStore> = (set, get, store) => {
     const words: WordsStore['words'] = [];
 
+    const loadWordsToWordsStoreFromDB: WordsStore['loadWordsToWordsStoreFromDB'] = function () {
+        set({ words: [] });
+    };
+    const addWord: WordsStore['addWord'] = function (word) {
+        const state = get();
+        const existingWord = state.words.find(value => value.spelling === word.spelling);
+        if (existingWord) {
+            showToastMessage('This word already exists');
+        } else {
+            set(state => ({ words: [word, ...state.words] }));
+        }
+    };
+    const editWord: WordsStore['editWord'] = function (word) {
+        const state = get();
+        const existingWord = state.words.find(value => value.spelling === word.spelling);
+        if (existingWord) {
+            existingWord.spelling = word.spelling;
+            existingWord.meaning = word.meaning;
+            existingWord.sentences = word.sentences;
+            set({ words: [...state.words] });
+        } else {
+            showToastMessage('Word not found');
+        }
+    };
+    const deleteWords: WordsStore['deleteWords'] = function (spellingsOfwordsToDelete) {
+        const filteredWords = get().words.filter(value => !spellingsOfwordsToDelete.includes(value.spelling));
+        set({ words: [...filteredWords] });
+    };
+
     return {
         words,
+        loadWordsToWordsStoreFromDB,
+        addWord,
+        editWord,
+        deleteWords,
     };
 };
